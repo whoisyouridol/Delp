28ec447c94a824e17814016b31ba97cd75c65493
whoisyouridol <misha.etmekchyan@gmail.com>
2023-09-27 14:26:54 +0400

    Add project files.

---
+++ Delp.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Delp.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delp.Infrastructure.Persistence
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Animal> Animals { get; set; }
+    }
+
+
+    public class Test
+    {
+        private void TestMethod()
+        {
+            var context = new ApplicationDbContext();
+        }
+    }
+}
